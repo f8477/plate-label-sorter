28184f77da2f88a334168e0b67625462eefc9468
fateme arabkhani <fatemearabkhani2005@gmail.com>
2026-07-25 14:30:11 +0330

    Update and fix bugs

--- plate_label_sorter.py
+++ plate_label_sorter.py
@@ -3,12 +3,13 @@ import shutil
 import re
 
 
-source_folder = r"D:\plate_data\plate_data"
-output_folder = r"D:\plate_data\sorted"
+source_folder = r"C:\Users\cactus.pc\Desktop\plate_label_sorter\plate_data\plate_data"
+output_folder = r"C:\Users\cactus.pc\Desktop\plate_label_sorter\sorted"
 
 
 os.makedirs(output_folder, exist_ok=True)
-
+total_input = len(os.listdir(source_folder))
+print("Total input files:", total_input)
 
 for image_name in os.listdir(source_folder):
 
@@ -18,29 +19,61 @@ for image_name in os.listdir(source_folder):
 
     match = re.search(r'\d+([a-zA-Z]+)\d+', image_name)
 
-
     if match:
         label = match.group(1)
 
+    elif "-" in image_name or "_" in image_name:
+        label = "-"
+
     else:
-        print("Label پیدا نشد:", image_name)
-        continue
+        label = "none"
 
 
-    label_folder = os.path.join(output_folder, label)
+    if label == "sat":
+        new_label = "sad"
+
+    elif label == "th":
+        new_label = "c"
+
+    elif label == "ghgh":
+        new_label = "gh"
+
+    else:
+        new_label = label
+
+
+
+    label_folder = os.path.join(output_folder, new_label)
 
     os.makedirs(label_folder, exist_ok=True)
 
 
+
+    new_image_name = image_name.replace(label, new_label)
+
+
     source_path = os.path.join(source_folder, image_name)
 
-    destination_path = os.path.join(label_folder, image_name)
+    destination_path = os.path.join(label_folder, new_image_name)
+
+    # overwrite
+    if os.path.exists(destination_path):
+
+        name, ext = os.path.splitext(new_image_name)
+
+        counter = 1
+
+        while os.path.exists(destination_path):
+            new_image_name = f"{name}_{counter}{ext}"
+            destination_path = os.path.join(label_folder, new_image_name)
+            counter += 1
 
 
     shutil.copy(source_path, destination_path)
 
 
-    print(image_name, "---->", label)
+    print(image_name, "---->", new_label)
+
 
 
 print("**************************************************************")
@@ -55,10 +88,21 @@ for class_name in sorted(os.listdir(output_folder)):
 
         count = len([
             img for img in os.listdir(class_path)
-            if img.lower().endswith(".jpg")
+            if os.path.isfile(os.path.join(class_path, img))
         ])
 
-        print(f"کلاس {class_name}: {count} عکس")
+        print(f"class {class_name}: {count} picture")
 
+
+print("\nChecking total output images...")
+total_output = 0
+
+for root, dirs, files in os.walk(output_folder):
+    for file in files:
+        if file.lower().endswith((".jpg", ".jpeg", ".png")):
+            total_output += 1
+
+
+print("Total output images:", total_output)
 
 print("\nDone :))")
